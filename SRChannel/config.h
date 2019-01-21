f1b96c3bb572cdd7da73e17670fb7db4ce8cb07a
johannesmenzel <johannes.menzel@posteo.de>
2019-01-21 23:22:28 +0100

    changed version strings to v0.2.1-alpha

--- SRChannel/config.h
+++ SRChannel/config.h
@@ -1,7 +1,7 @@
 #define PLUG_NAME "SRChannel"
 #define PLUG_MFR "SRPlugins"
-#define PLUG_VERSION_HEX 0x00000200
-#define PLUG_VERSION_STR "0.2.0"
+#define PLUG_VERSION_HEX 0x00000201
+#define PLUG_VERSION_STR "0.2.1"
 #define PLUG_UNIQUE_ID 'SrCn'
 #define PLUG_MFR_ID 'ShRa'
 #define PLUG_URL_STR "https://github.com/johannesmenzel"
